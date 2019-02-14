b9e208ef26d1ba532ce6ae2d2966f4e54d7970f3
epicTCK <caleblgentry@gmail.com>
2019-02-14 18:07:27 -0500

    basic test

---
+++ bot.py
@@ -0,0 +1,9 @@
+from flask import Flask, request
+app = Flask(__name__)
+
+@app.route('/post', methods = ["POST"])
+def post():
+	print(request.data)
+	return ''
+
+app.run(host='0.0.0.0', port=33507)
