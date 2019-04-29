ddfefcca3ed1dd5e713ba877177c01f6dfeaef03
Caleb <caleblgentry@gmail.com>
2019-04-29 06:07:10 +0000

    test

--- bot.py
+++ bot.py
@@ -1,12 +1,15 @@
 from flask import Flask, request
 import requests
 import os
+import json
+
 app = Flask(__name__)
 
 @app.route('/post', methods = ['POST'])
 def post():
-	requests.post("https://api.groupme.com/v3/bots/post",{"text" : request.Form["text"], "bot_id" : "475117f0e94dd02bd7c7e380fc"})
-	print(request.data)
+	responseInput = request.get_json()
+	if responseInput['name'] != 'testBot':
+		requests.post("https://api.groupme.com/v3/bots/post",{"text" : responseInput['text'], "bot_id" : "475117f0e94dd02bd7c7e380fc"})
 	return ''
 
 app.run(host='0.0.0.0', port=os.environ.get('PORT'))
