32af10bf3a9bf65abea154315a2bc4cc312f164a
Caleb <caleblgentry@gmail.com>
2019-04-29 04:42:32 +0000

    yay

--- bot.py
+++ bot.py
@@ -5,7 +5,7 @@ app = Flask(__name__)
 
 @app.route('/post', methods = ['POST'])
 def post():
-	requests.post("https://api.groupme.com/v3/bots/post",{"text" : request.text, "bot_id" : "475117f0e94dd02bd7c7e380fc"})
+	requests.post("https://api.groupme.com/v3/bots/post",{"text" : requests.data, "bot_id" : "475117f0e94dd02bd7c7e380fc"})
 	print(request.data)
 	return ''
 
